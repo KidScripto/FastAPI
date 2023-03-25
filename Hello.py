e8d519dac656d5f5669d54b72515e80087590ca6
Drew Meylan <kidscripto@pop-os.localdomain>
2023-03-25 19:59:13 -0400

    Updated Hello.py; working from freeCodeCamp course

--- Hello.py
+++ Hello.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+## Resume 39.24
+## https://www.youtube.com/watch?v=0sOvCWFmrtA&t=730s
